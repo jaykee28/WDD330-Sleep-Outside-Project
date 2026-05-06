eb4ec445bea0ec4c9c33f5ab419664cd0b9eeea1
jaykee28 <jmufetu@gmail.com>
2026-05-06 23:12:35 +0200

    Refactored product pages to dynamic product detail pages

--- src/js/product.js
+++ src/js/product.js
@@ -1,26 +1,11 @@
-import { getLocalStorage, setLocalStorage } from './utils.mjs';
+import { getParam } from './utils.mjs';
 import ProductData from './ProductData.mjs';
+import ProductDetails from './ProductDetails.mjs';
+
+const productId = getParam('product');
 
 const dataSource = new ProductData('tents');
 
-function addProductToCart(product) {
-  let cart = getLocalStorage('so-cart') || [];
+const product = new ProductDetails(productId, dataSource);
 
-  if (!Array.isArray(cart)) {
-    cart = [cart];
-  }
-
-  cart.push(product);
-
-  setLocalStorage('so-cart', cart);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById('addToCart')
-  .addEventListener('click', addToCartHandler);
+product.init();
